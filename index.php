7b915d764798434d8c767b5e950555ae6b34fe07
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-09-07 19:59:37 +0200

    [UPD] milestone 1 completed

--- index.php
+++ index.php
@@ -39,14 +39,23 @@ if (isset($_GET['lunghezza'])) {
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <form action="./index.php" method="GET">
-                    <div class="row">
-                        <div class="col-6">
-                            <h3>Premi il pulsante per generare una password randomica</h3>
-                            <button type="button" class="btn btn-primary">Primary</button>
-                        </div>
-                    </div>
-                </form>
+                <h1 class="m-auto mb-5">Generatore di Password Sicura</h1>
+                <!-- Form per l'invio della lunghezza della password -->
+                <div class="mb-5">
+                    <form method="GET" action="">
+                        <label for="lunghezza">Lunghezza password:</label>
+                        <input type="number" id="lunghezza" name="lunghezza" min="1" max="100" required>
+                        <button type="button">Genera Password</button>
+                    </form>
+                </div>
+                <div class="mb-5">
+                    <?php if (!empty($password_generata)): ?>
+                        <h2>La tua password generata è:</h2>
+                        <p><strong><?php echo $password_generata; ?></strong></p>
+                        <?php elseif (isset($_GET['lunghezza']) && $lunghezza <= 0): ?>
+                            <p>Inserisci una lunghezza valida.</p>
+                        <?php endif; ?>
+                </div>
             </div>
         </div>
     </div>
